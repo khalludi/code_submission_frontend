97122e2b344eebf1084a9eb1845defc53e9f9c11
Khalid Ali <khalludi123@gmail.com>
2024-07-01 00:31:11 -0400

    feat: display error to user when code fails to run
    feat: fix overflow issue when code section gets too long

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import CodeForm from "@/components/CodeForm";
 
 export default function Home() {
   return (
-    <main className="grid grid-cols-2 gap-4 min-h-screen">
+    <main className="grid grid-cols-2 gap-4 h-screen w-full p-4 overflow-hidden">
       <div className="p-10 bg-gray-400 rounded-2xl">
         <h2>Problem Statement</h2>
       </div>
--- components/CodeForm.tsx
+++ components/CodeForm.tsx
@@ -9,40 +9,33 @@ import {runCode} from "@/app/actions";
 const initialState = { output: '', }
 
 export default function CodeForm() {
-  const [code, setCode] = useState(
-    `function add(a, b) {\n  return a + b;\n}`
-  );
-
   const [state, formAction] = useFormState(runCode, initialState);
 
   return (
-    <form action={formAction}>
-      <div className='grid grid-rows-2 gap-4 h-screen'>
-        <div className="rounded-2xl p-10 bg-blue-300 flex flex-col gap-4">
-          <h2>Code</h2>
+    <form action={formAction} className="h-full overflow-hidden flex flex-col gap-4">
+      <div className="rounded-2xl p-10 bg-blue-300 flex flex-col gap-4" style={{ height: "64%" }}>
+        <h2>Code</h2>
 
-          <div style={{ overflow: "auto" }} className="h-auto">
-            <CodeEditor
-              name="code"
-              language="py"
-              placeholder="Please enter Python code."
-              onChange={(evn) => setCode(evn.target.value)}
-              padding={15}
-              style={{
-                backgroundColor: "#f5f5f5",
-                fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
-              }}
-            />
-          </div>
-
-          <Button type="submit">Run Code</Button>
+        <div style={{ overflow: "auto" }} className="h-auto">
+          <CodeEditor
+            name="code"
+            language="py"
+            placeholder="Please enter Python code."
+            padding={15}
+            style={{
+              backgroundColor: "#f5f5f5",
+              fontFamily: 'ui-monospace,SFMono-Regular,SF Mono,Consolas,Liberation Mono,Menlo,monospace',
+            }}
+          />
         </div>
 
-        <div className="rounded-2xl p-10 bg-amber-50">
-          <h2>Output</h2>
-          <p>{state.output}</p>
-        </div>
+        <Button type="submit">Run Code</Button>
+      </div>
+
+      <div className="rounded-2xl p-10 bg-amber-500 h-1/3">
+        <h2>Output</h2>
+        <p className="whitespace-pre text-wrap">{state.output}</p>
       </div>
     </form>
-);
+  );
 }
